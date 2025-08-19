dad0723443e9ef3bb567bda6270c22b4da92faaa
Jenso69 <antheacaruana8@gmail.com>
2025-08-19 18:12:03 +0200

    Idle animation done, fixing running

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    private Animator animator;
     private Vector2 movement;
 
-    // Update is called once per frame
+    void Start()
+    {
+        
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         float input = Input.GetAxis("Horizontal");
         movement.x = input * speed * Time.deltaTime;
-        transform.Translate(movement);
+
+        
+        transform.Translate(new Vector3(movement.x, 0f, 0f));
+
+        
+        animator.SetBool("isRunning", input != 0);
+        Debug.Log("IsRunning = " + (input != 0));
+        
+        if (input < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (input > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
+
+
+
