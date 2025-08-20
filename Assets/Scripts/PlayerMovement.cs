16e412b9b1fa410df77e0eaff9166cd91dd467d6
Jenso69 <antheacaruana8@gmail.com>
2025-08-20 15:37:44 +0200

    Added Jump and Fall animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,38 +2,80 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 4f;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
     private Animator animator;
-    private Vector2 movement;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    public int moveDirection { get; private set; }
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float input = Input.GetAxis("Horizontal");
-        movement.x = input * speed * Time.deltaTime;
+        float input = Input.GetAxisRaw("Horizontal"); // -1, 0, 1
 
-        
-        transform.Translate(new Vector3(movement.x, 0f, 0f));
+        // Move player
+        rb.linearVelocity = new Vector2(input * speed, rb.linearVelocity.y);
 
-        
+        // Save movement direction
+        moveDirection = (int)input;
+
+        // Running animation
         animator.SetBool("IsRunning", input != 0);
-        Debug.Log("IsRunning = " + (input != 0));
-        
+
+        // Flip sprite
         if (input < 0)
-        {
             transform.localScale = new Vector3(-1, 1, 1);
-        }
         else if (input > 0)
-        {
             transform.localScale = new Vector3(1, 1, 1);
+
+        // Jump input
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        // --- ANIMATION STATES BASED ON VELOCITY ---
+        if (!isGrounded && rb.linearVelocity.y > 0.1f)
+        {
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsFalling", false);
+        }
+        else if (!isGrounded && rb.linearVelocity.y < -0.1f)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+        }
+        else if (isGrounded)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
-
 
 
