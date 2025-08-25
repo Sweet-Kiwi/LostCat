66f3daffc0baad85e3d260c8c03722986ee95d26
Jenso69 <antheacaruana8@gmail.com>
2025-08-25 18:43:12 +0200

    Collectible particles

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -3,13 +3,23 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public int points = 1; // How many points this collectible is worth
+    public ParticleSystem collectEffect; // Assign the particle system in Inspector
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             // You could add points to a score manager here
-            Debug.Log("Collected!");
+            Debug.Log("Collected! +" + points + " points");
+
+            // Play particle effect if assigned
+            if (collectEffect != null)
+            {
+                // Detach the particle system so it can play after destroying the collectible
+                collectEffect.transform.parent = null;
+                collectEffect.Play();
+                Destroy(collectEffect.gameObject, collectEffect.main.duration);
+            }
 
             // Destroy the collectible
             Destroy(gameObject);
